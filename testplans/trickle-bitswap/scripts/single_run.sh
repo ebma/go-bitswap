aaed907b39c53c11436438ab4b3b7c74386b24c9
Marcel Ebert <mail@marcel-ebert.de>
2022-08-20 21:35:11 +0200

    Update script

--- testplans/trickle-bitswap/scripts/single_run.sh
+++ testplans/trickle-bitswap/scripts/single_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-RUNNER="local:docker"
-BUILDER="docker:go"
-#RUNNER="local:exec"
-#BUILDER="exec:go"
+#RUNNER="local:docker"
+#BUILDER="docker:go"
+RUNNER="local:exec"
+BUILDER="exec:go"
 
 echo "Cleaning previous results..."
 
@@ -13,7 +13,7 @@ mkdir ./results
 FILE_SIZE=15728640
 #FILE_SIZE=15728640,31457280,47185920,57671680
 RUN_COUNT=2
-INSTANCES=5
+INSTANCES=10
 LEECH_COUNT=3
 PASSIVE_COUNT=0
 LATENCY=10
@@ -21,10 +21,10 @@ JITTER=10
 BANDWIDTH=150
 PARALLEL_GEN=100
 TESTCASE=bitswap-transfer
-#INPUT_DATA=files
-INPUT_DATA=random # does not work locally due to permissions
-DATA_DIR=../extra/test-datasets # This would be for the docker:go runner
-#DATA_DIR=/home/marcel/Documents/Studies/thesis/trickle-bitswap/go-bitswap/testplans/trickle-bitswap/test-datasets # This is for the local runner
+INPUT_DATA=files # use this for local:exec
+#INPUT_DATA=random # use this for local:docker || does not work locally due to permissions
+#DATA_DIR=../extra/test-datasets # This would be for the docker:go runner
+DATA_DIR=/home/marcel/Documents/Studies/thesis/trickle-bitswap/go-bitswap/testplans/trickle-bitswap/test-datasets # This is for the local:exec runner
 TCP_ENABLED=false
 MAX_CONNECTION_RATE=100
 
