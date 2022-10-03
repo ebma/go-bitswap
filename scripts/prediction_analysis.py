613ce95c76d6046e3ed567ce37f4371487728646
Marcel Ebert <mail@marcel-ebert.de>
2022-10-03 18:12:15 +0200

    Remove dummy data from script

--- scripts/prediction_analysis.py
+++ scripts/prediction_analysis.py
@@ -14,47 +14,26 @@ def analyse_and_save_to_file(results_dir, export_pdf):
     message_items = first_timestamp_estimator.aggregate_message_histories(results_dir)
 
     # Holds the items that describe the true target of the prediction
-    predictionTargets = [item for item in info_items if item['type'] == 'LeechInfo']
+    prediction_targets = [item for item in info_items if item['type'] == 'LeechInfo']
 
-    byLatency = process.groupBy(message_items, "latencyMS")
+    by_latency = process.groupBy(message_items, "latencyMS")
     # Get prediction rates for each latency (contains multiple experiments)
-    for latency, messagesWithLatency in byLatency.items():
+    for latency, messagesWithLatency in by_latency.items():
         # Group by experiments
-        byLatencyAndExperiment = process.groupBy(messagesWithLatency, "experiment")
-        for experiment, messagesWithExperiment in byLatencyAndExperiment.items():
-            byTricklingDelayWithLatencyAndExperiment = process.groupBy(messagesWithExperiment, "tricklingDelay")
+        by_latency_and_experiment = process.groupBy(messagesWithLatency, "experiment")
+        for experiment, messagesWithExperiment in by_latency_and_experiment.items():
+            by_trickling_delay_with_latency_and_experiment = process.groupBy(messagesWithExperiment, "tricklingDelay")
 
             # Group by delay
-            for delay, messagesWithDelay in byTricklingDelayWithLatencyAndExperiment.items():
-                targets = [item for item in predictionTargets if
+            for delay, messagesWithDelay in by_trickling_delay_with_latency_and_experiment.items():
+                targets = [item for item in prediction_targets if
                            item['experiment'] == experiment and item['latencyMS'] == latency and item[
                                'tricklingDelay'] == delay]
                 rate = first_timestamp_estimator.get_prediction_rate(messagesWithDelay, targets)
-                # results[latency][experiment][delay] = rate
                 results.append({'latency': latency, 'experiment': experiment, 'delay': delay, 'rate': rate})
-                # if latency not in results:
-                #     results[latency] = list()
-                # results[latency].append(
-                #     {'experiment': experiment, 'delay': delay, 'rate': rate})
 
-    # artificially amend results
-    def map_function(x, modifier, name):
-        y = {}
-        y['rate'] = x['rate'] + modifier
-        y['experiment'] = name
-        y['delay'] = x['delay']
-        y['latency'] = str(int(x['latency']) + 50)
-        return y
-
-    other_results = list(map(lambda x: map_function(x, 0.1, 'asdf'), results))
-    results.extend(other_results)
-    other_results = list(map(lambda x: map_function(x, -0.1, 'befg'), results))
-    results.extend(other_results)
-    other_results = list(map(lambda x: map_function(x, -0.2, 'xyz'), results))
-    results.extend(other_results)
-
-    df = pd.DataFrame({'Delay': [item['delay'] for item in results], \
-                       'Latency (ms)': [item['latency'] for item in results], \
+    df = pd.DataFrame({'Delay': [item['delay'] for item in results],
+                       'Latency (ms)': [item['latency'] for item in results],
                        'Rate': [item['rate'] for item in results]}, dtype=float)
 
     dd = pd.melt(df, id_vars=['Delay', 'Latency (ms)'], value_vars=['Rate'])
