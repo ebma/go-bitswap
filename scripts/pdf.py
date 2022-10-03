8f91eb4411c4cbbbd1e8b366367f6eb22a87978b
Marcel Ebert <mail@marcel-ebert.de>
2022-10-03 18:22:20 +0200

    Make script save plots to different files

--- scripts/pdf.py
+++ scripts/pdf.py
@@ -1,9 +1,11 @@
-import process
 import os
 import sys
+
+from matplotlib.backends.backend_pdf import PdfPages
+
 import first_timestamp_estimator
 import prediction_analysis
-from matplotlib.backends.backend_pdf import PdfPages
+import process
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 filename = "result.pdf"
@@ -11,27 +13,29 @@ if len(sys.argv) == 2:
     filename = sys.argv[1] + ".pdf"
 
 results_dir = dir_path + "/../../experiments/results"
+target_dir = dir_path + "/../../experiments"
 
-with PdfPages(dir_path + "/../../experiments/prediction_rates.pdf") as export_pdf:
+agg, testcases = process.aggregate_results(results_dir)
+byLatency = process.groupBy(agg, "latencyMS")
+byNodeType = process.groupBy(agg, "nodeType")
+byFileSize = process.groupBy(agg, "fileSize")
+byBandwidth = process.groupBy(agg, "bandwidthMB")
+byTopology = process.groupBy(agg, "topology")
+byTricklingDelay = process.groupBy(agg, "tricklingDelay")
+
+with PdfPages(target_dir + "/" + "prediction_rates.pdf") as export_pdf:
     prediction_analysis.analyse_and_save_to_file(results_dir, export_pdf)
 
-with PdfPages(dir_path + "/../../experiments/" + filename) as export_pdf:
-    agg, testcases = process.aggregate_results(results_dir)
-    byLatency = process.groupBy(agg, "latencyMS")
-    byNodeType = process.groupBy(agg, "nodeType")
-    byFileSize = process.groupBy(agg, "fileSize")
-    byBandwidth = process.groupBy(agg, "bandwidthMB")
-    byTopology = process.groupBy(agg, "topology")
-    byTricklingDelay = process.groupBy(agg, "tricklingDelay")
-
-    # There will only be one latency and topology in the results
-    latency = list(byLatency.keys())[0]
-    topology = list(byTopology.keys())[0]
-
+with PdfPages(target_dir + "/" + "prediction_rates_agg.pdf") as export_pdf:
     first_timestamp_estimator.plot_estimate(results_dir)
     export_pdf.savefig()
+
+with PdfPages(target_dir + "/" + "time-to-fetch.pdf") as export_pdf:
+    latency = list(byLatency.keys())[0]
     process.plot_trickling_delays(latency, byTricklingDelay)
     export_pdf.savefig()
-    # export_pdf.savefig(pad_inches=0.4, bbox_inches='tight')
+
+with PdfPages(target_dir + "/" + "messages.pdf") as export_pdf:
+    topology = list(byTopology.keys())[0]
     process.plot_messages(topology, byTricklingDelay)
     export_pdf.savefig()
