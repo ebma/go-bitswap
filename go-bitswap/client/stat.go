e7f09f1ec288de0567e04f2ca7f62160a052f048
Marcel Ebert <mail@marcel-ebert.de>
2022-10-08 12:01:34 +0200

    Remove stat changes

--- go-bitswap/client/stat.go
+++ go-bitswap/client/stat.go
@@ -12,16 +12,10 @@ type Stat struct {
 	DupBlksReceived  uint64
 	DupDataReceived  uint64
 	MessagesReceived uint64
-
-	BlocksSent    uint64
-	DataSent      uint64
-	Peers         []string
-	ProvideBufLen int
 }
 
 // Stat returns aggregated statistics about bitswap operations
 func (bs *Client) Stat() (st Stat, err error) {
-	//st.ProvideBufLen = len(bs.newBlocks)
 	st.Wantlist = bs.GetWantlist()
 	bs.counterLk.Lock()
 	c := bs.counters
@@ -32,16 +26,6 @@ func (bs *Client) Stat() (st Stat, err error) {
 	st.MessagesReceived = c.messagesRecvd
 	bs.counterLk.Unlock()
 	st.Wantlist = bs.GetWantlist()
-
-	//st.BlocksSent = c.blocksSent
-	//st.DataSent = c.dataSent
-	//peers := bs.engine.Peers()
-	//st.Peers = make([]string, 0, len(peers))
-	//
-	//for _, p := range peers {
-	//	st.Peers = append(st.Peers, p.Pretty())
-	//}
-	//sort.Strings(st.Peers)
 
 	return st, nil
 }
