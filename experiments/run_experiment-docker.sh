74a1494128fb477472586ccc538d6f4834c871a3
Marcel Ebert <mail@marcel-ebert.de>
2022-10-08 12:21:09 +0200

    Change composition files for experiments

--- experiments/run_experiment-docker.sh
+++ experiments/run_experiment-docker.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 
-echo "Cleaning previous results..."
 RUNNER="local:docker"
 
-rm -rf ./results
-mkdir ./results
+mkdir ../../experiments/results
 
 source ../scripts/exec.sh
 
 echo "[*] Running Experiment"
-run_composition ./composition-docker.toml
+run_composition ./composition-docker-4-eaves.toml
 # Plot in pdf
 python3 ../scripts/pdf.py
-# Check adversary predictions
-python3 ../scripts/first-timestamp-estimator.py -rdir=../experiments/results
-
-#echo "Cleaning previous results..."
-#rm -rf ./results
-#mkdir ./results
-#
-#echo "[*] Running baseline"
-#run_composition ./baseline-docker.toml
-## Plot in pdf
-#python3 ../scripts/pdf.py baseline
-## Check adversary predictions
-#python3 ../scripts/first-timestamp-estimator.py -rdir=../experiments/results
--- experiments/run_experiment-local.sh
+++ experiments/run_experiment-local.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo "Cleaning previous results..."
 RUNNER="local:exec"
 
-rm -rf ./results
-mkdir ./results
+mkdir ../../experiments/results
 
 source ../scripts/exec.sh
 
@@ -12,16 +10,3 @@ echo "[*] Running Experiment"
 run_composition ./composition-local.toml
 # Plot in pdf
 python3 ../scripts/pdf.py
-# Check adversary predictions
-python3 ../scripts/first-timestamp-estimator.py -rdir=../experiments/results
-
-#echo "Cleaning previous results..."
-#rm -rf ./results
-#mkdir ./results
-#
-#echo "[*] Running baseline"
-#run_composition ./baseline-local.toml
-## Plot in pdf
-#python3 ../scripts/pdf.py baseline
-## Check adversary predictions
-#python3 ../scripts/first-timestamp-estimator.py -rdir=../experiments/results
