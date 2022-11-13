165be45944cdcd4df7f2c551d6396ec96958a645
Marcel Ebert <mail@marcel-ebert.de>
2022-11-13 17:13:19 +0100

    Change scripts

--- experiments/run_experiment-baseline.sh
+++ experiments/run_experiment-baseline.sh
@@ -6,7 +6,7 @@ mkdir ../../experiments/results-baseline
 
 source ../scripts/exec.sh
 
-echo "[*] Running Experiment"
+echo "[*] Running Experiment ./composition-docker-baseline.toml"
 run_composition ./composition-docker-baseline.toml
 # Plot in pdf
 python3 ../scripts/pdf.py
--- experiments/run_experiment-docker-eaves.sh
+++ experiments/run_experiment-docker-eaves.sh
@@ -6,7 +6,7 @@ mkdir ../../experiments/results
 
 source ../scripts/exec.sh
 
-echo "[*] Running Experiment"
-run_composition ./composition-docker-7-eaves.toml
+echo "[*] Running Experiment for file ./composition-docker-$1-eaves.toml"
+run_composition ./composition-docker-"$1"-eaves.toml
 # Plot in pdf
 python3 ../scripts/pdf.py
